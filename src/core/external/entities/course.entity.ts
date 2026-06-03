ceb769e5918cc362b0a09d0dfab7804b2dc28b6c
Red-Hide <guillaume.ferrand67@gmail.com>
2026-06-03 18:28:23 +0200

    Remove external entities that doesn't need to be there
    Oopsie

--- src/core/external/entities/course.entity.ts
+++
@@ -1,5 +0,0 @@
-import { Entity } from 'typeorm';
-import { InternalCourse } from '@dad-group-1/backend-common';
-
-@Entity()
-export class Course extends InternalCourse {}
--- src/core/external/entities/user.entity.ts
+++
@@ -1,5 +0,0 @@
-import { Entity } from 'typeorm';
-import { InternalUser } from '@dad-group-1/backend-common';
-
-@Entity()
-export class User extends InternalUser {}
